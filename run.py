4dd4ddd4fc410d5a4b3f0a254a6abcdc3cd6eb30
mijdavis2 <mdavisinsc@gmail.com>
2016-04-28 07:56:39 -0700

    :up: Refactor dev_utils so they can be tested

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ from starter_weppy import app
 
 @contextmanager
 def run_in_dev():
-    from starter_weppy.dev_utils import remove_test_users
+    from starter_weppy.dev_utils import remove_admin, remove_user
     try:
         yield
     finally:
-        remove_test_users()
+        remove_admin()
+        remove_user()
 
 
 if __name__ == "__main__":
@@ -18,8 +19,10 @@ if __name__ == "__main__":
                             action='store_true')
     args = arg_parser.parse_args()
     if args.dev:
-        from starter_weppy.dev_utils import setup_test_users
-        setup_test_users()
+        from starter_weppy.dev_utils import setup_admin, setup_user
+        test_admin = setup_admin()
+        test_user = setup_user()
+        print("Admin: {} \nUser: {}\n".format(test_admin.as_dict(), test_user.as_dict()))
         with run_in_dev():
             app.run()
     else:
